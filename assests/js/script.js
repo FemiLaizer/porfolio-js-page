db6c08ff4cac8fdc34b7f892f3fba7696ba10c41
Femi-Laizer <59768131+FemiLaizer@users.noreply.github.com>
2022-08-03 19:18:37 +0100

    Add files via upload

---
+++ assests/js/script.js
@@ -0,0 +1,14 @@
+/* ==== Show Menu ==== */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle("show")
+        })
+    }
+}
+showMenu("nav-toggle", "nav-menu")
+
+/* ==== Active and Remove Menu ==== */
